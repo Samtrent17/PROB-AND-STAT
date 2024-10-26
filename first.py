ce986cd243e0a80984ae77fb481ee1024db46ef1
Samtrent17 <xamsamuel079@gmail.com>
2024-10-26 11:52:13 +0300

    practise for prob and stats

---
+++ first.py
@@ -0,0 +1,37 @@
+#NANGOLI SAMUEL
+#M24B13/039
+#B28338
+
+
+#commenting out 
+
+'''
+commenting out mulitple lines
+
+'''
+
+#declaring variables
+
+name = 'Trent'
+number_one = 23
+number_one_two = 45
+status = False
+
+print(type(number_one))
+print(type(number_one_two))
+print(type(status))
+
+print(number_one + number_one_two)
+
+def subtraction():
+    difference = number_one - number_one_two
+    return difference
+print(subtraction())
+
+
+## a void function
+
+def addition():
+    sum = number_one + number_one_two
+    print(sum)
+addition()
